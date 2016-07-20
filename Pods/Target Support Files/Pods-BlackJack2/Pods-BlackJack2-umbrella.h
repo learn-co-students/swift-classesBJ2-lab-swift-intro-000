87190bfda0cf32bf03719539b599510c3bf6125e
JimCampagno <jimcampagno@gmail.com>
2016-07-20 13:48:23 -0400

    Include Quick & Nimble

---
+++ Pods/Target Support Files/Pods-BlackJack2/Pods-BlackJack2-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_BlackJack2VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BlackJack2VersionString[];
+
---
+++ Pods/Target Support Files/Pods-BlackJack2Tests/Pods-BlackJack2Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BlackJack2Tests : NSObject
+@end
+@implementation PodsDummy_Pods_BlackJack2Tests
+@end
