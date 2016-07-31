1fdacb59f537dd1a9c056c884d72ac0525154f98
Michael Dippery <michael@monkey-robot.com>
2016-07-30 21:11:05 -0700

    Lay out UI

--- BlackJack2/ViewController.swift
+++ BlackJack2/ViewController.swift
@@ -9,17 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var winnerLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func playGamePressed(sender: UIButton) {
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
