ff0b9e4b1943e27fc20c547c66a7607fdf3a1613
chenjm1960 <chenjm1960@yahoo.com>
2018-02-19 23:45:46 -0800

    finished

--- BlackJack2/ViewController.swift
+++ BlackJack2/ViewController.swift
@@ -13,26 +13,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var winnerLabel: UILabel!
     
     @IBAction func button(sender: UIButton) {
+        
+        playGame()
+        
     }
     
     
     var dealer = Dealer()
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
     func playGame() -> Player {
         var turn = "player"
         dealer.deal()
-
+        
         if let _ = dealer.winner {
             dealer.award()
         }
-
+        
         while dealer.winner == nil {
             let player = turn == "player" ? dealer.house : dealer.player
             dealer.turn(player)
             turn = turn == "player" ? "house" : "player"
         }
-
+        
         dealer.award()
+        winnerLabel.text = ("\(dealer.winner!.name)")
         return dealer.winner!
     }
 
